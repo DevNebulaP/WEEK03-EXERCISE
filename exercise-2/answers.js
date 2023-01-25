636dc6e33df03a5abf8455e62dab798490f29649
DevNebulaP <76225632+DevNebulaP@users.noreply.github.com>
2023-01-25 22:27:58 +0700

    finished exercise 1-2

--- exercise-2/answers.js
+++ exercise-2/answers.js
@@ -1,19 +1,15 @@
 function convertADtoBE(input) {
-    // AD คือคริสดศักราย
-    // BE คือพุทธศักราช
-    //
-    // TODO: แปลงปีใน คริสตศักราช เป็น พุทธศักราช เช่น 2000 เป็น "พ.ศ. 2543"
-    // โดยให้เติมตัวอักษร พ.ศ. เข้าไปด้านหน้าด้วย
+    return (typeof(input) == 'number' && input >= 0)? "พ.ศ. " + (input+543) : "ไม่ถูกต้อง"
 }
 
 function evenOrOdd(input) {
-    // TODO: ให้ตรวจสอบว่าตัวเลข input เป็นเลขคู่หรือเลขคี่
+    return input%2 == 0? "even" : "odd"
 }
 
 function getFullName(input) {
-    // TODO: ให้นำคำนำหน้าชื่อ ชื่อต้น นามสกุล มาต่อกัน
+    return (input["sex"] == "male"? "Mr. " : "Ms. ") + input["firstName"] + " " + input["lastName"]
 }
 
 function getFirstName(input) {
-    // TODO: ให้ทำการตัดนามสกุลออกโดยใช้ indexOf() และ substring()
+    return input.substring(0, input.indexOf(" "))
 }
